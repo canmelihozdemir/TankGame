5c8ac243acbf44d376dbe064fd79e43339827f62
MelihOzdemir <canmelihozdemir@gmail.com>
2021-05-28 19:37:35 +0300

    + added InputSystem

---
+++ TankGame/Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame.PlayerInput
+{
+    [CreateAssetMenu(menuName = "TankGame/UserInput/UserInput Settings", fileName = "UserInputSettings")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+
+}
