180af8893a215da395601c37d4d5feb3fbe96975
MelihOzdemir <canmelihozdemir@gmail.com>
2021-05-28 19:37:46 +0300

    + added MovementSystem

---
+++ TankGame/Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TankGame.PlayerInput;
+using UnityEngine;
+
+namespace TankGame.PlayerMovement
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private PlayerMovementSettings _movementSettings;
+
+        private void Update()
+        {
+            _rigidbody.MovePosition(_rigidbody.position+(_rigidbody.transform.forward*_inputData.Vertical* _movementSettings.VerticalSpeed));
+            _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.right*_inputData.Horizontal*_movementSettings.HorizontalSpeed));
+        }
+    }
+
+}
---
+++ TankGame/Assets/TopDownShooter/Scripts/Movement/PlayerMovementSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame.PlayerMovement
+{
+    [CreateAssetMenu(menuName = "TankGame/Player/Movement Settings", fileName = "PlayerMovementSettings")]
+    public class PlayerMovementSettings : ScriptableObject
+    {
+        public float HorizontalSpeed=5f;
+        public float VerticalSpeed=5f;
+    }
+
+}
