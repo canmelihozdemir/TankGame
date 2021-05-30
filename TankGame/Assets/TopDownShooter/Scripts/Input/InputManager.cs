5a0aef77006fe53a8e8cf9181c873bd5722d2a9e
MelihOzdemir <canmelihozdemir@gmail.com>
2021-05-30 12:29:32 +0300

    + fixed layer and physics system in the game

--- TankGame/Assets/TopDownShooter/Scripts/Input/InputManager.cs
+++ TankGame/Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -7,12 +7,16 @@ namespace TankGame.PlayerInput
     public class InputManager : MonoBehaviour
     {
         [SerializeField] private InputData _inputData;
+        [SerializeField] private InputData _rotationInputData;
+        private Vector3 _lastInputData;
 
 
         private void Update()
         {
             _inputData.Horizontal = Input.GetAxis("Horizontal");
             _inputData.Vertical = Input.GetAxis("Vertical");
+
+            _rotationInputData.Horizontal = Input.mousePosition.x - _lastInputData.x;
         }
     }
 
---
+++ TankGame/Assets/TopDownShooter/Scripts/Movement/PlayerRotationController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TankGame.PlayerInput;
+using UnityEngine;
+
+namespace TankGame.PlayerMovement
+{
+    public class PlayerRotationController : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private Transform _tower;
+
+        private void Update()
+        {
+            _tower.Rotate(0,_inputData.Horizontal,0,Space.Self);
+        }
+    }
+
+}
